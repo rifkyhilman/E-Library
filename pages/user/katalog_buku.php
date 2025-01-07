16ef9f1bc9e79d1c55e00220af4aec7a24ab7a52
rifkyhilman <rifkyhilman265@gmail.com>
2025-01-07 18:33:37 +0700

    :white_check_mark: Search buku done
    search buku done

--- pages/user/katalog_buku.php
+++ pages/user/katalog_buku.php
@@ -9,46 +9,67 @@
         </nav>                          
     </div>
     <div class="box-page">
-        <div class="input-group">
-            <span class="input-group-text bg-transparent" id="basic-addon1">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </span>
-            <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Cari Judul Buku...">
-        </div>
-        <datalist id="datalistOptions">
-            <?php
-                $sql = $koneksi->query("SELECT * from tb_buku");
-                while ($data= $sql->fetch_assoc()) {
-            ?>
-                <option value="<?php echo $data['judul_buku']; ?>">
-            <?php 
-                }
-            ?>
-        </datalist>
+        <form action="" method="POST">
+            <div class="input-group">
+                <span class="input-group-text bg-transparent" id="basic-addon1">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </span>
+                <input class="form-control" name="search" placeholder="Cari Judul Buku..." required>
+                <button type="submit" name="btnSearch" hidden></button>
+            </div>
+        </form>
     </div>
     <div class="row row-cols-1 row-cols-md-2 g-4">
         <?php
-            $sql = $koneksi->query("SELECT * from tb_buku");
-            while ($data= $sql->fetch_assoc()) {
-        ?>
-        <div class="col">
-            <div class="card">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="images/buku/<?php echo $data['gambar']; ?>" class="img-fluid rounded-start" alt="Sampul Buku">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $data['judul_buku']; ?></h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            if (isset($_POST['btnSearch'])) {  
+                $filter_value= $_POST['search'];
+
+                $query="SELECT * FROM tb_buku WHERE judul_buku LIKE '%$filter_value%'";
+                $query_run = mysqli_query($koneksi, $query);
+
+                if(mysqli_num_rows($query_run) > 0){
+                    foreach($query_run as $data_buku){?>
+                        <div class="card"  style="max-width: 600px;">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="images/buku/<?php echo $data_buku['gambar']; ?>" class="img-fluid rounded-start" alt="Sampul Buku">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo $data_buku['judul_buku']; ?></h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php  
+                    }
+                }else {
+                    print_r("DATA Kaga ada di database");
+                }
+            } else {
+                $sql = $koneksi->query("SELECT * from tb_buku");         
+                while ($data = $sql->fetch_assoc()) { ?>        
+                    <div class="col">
+                        <div class="card">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img src="images/buku/<?php echo $data['gambar']; ?>" class="img-fluid rounded-start" alt="Sampul Buku">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php echo $data['judul_buku']; ?></h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <?php
+            <?php
+                } 
             }
-        ?>
+            ?>
     </div>
 </section>
